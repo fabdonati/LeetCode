406dbc86da39c5e892a04f134072187f5bda2a67
Fabrizio Donati <fabrizio.donati@protonmail.com>
2024-08-25 23:04:18 +0200

    Time: 119 ms (98.72%), Space: 16.3 MB (99.58%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        L, r = len(s), 0
+        for i in range(L):
+            for a,b in [(i,i),(i,i+1)]:
+                while a >= 0 and b < L and s[a] == s[b]: 
+                    a -= 1; b += 1
+                r += (b-a)//2 # (b-a)//2 is the number of palindromic substrings centered at a and b
+        return r
